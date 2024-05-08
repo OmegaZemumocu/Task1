f60bd76ad272dcec9757037165d49393fc8d4bb6
Daimons <Daimons@DESKTOP-3BPQG93>
2024-05-08 16:43:00 +0300

    Добавьте файлы проекта.

---
+++ Task1/Controllers/CalcController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Task1.Models;
+
+namespace Task1.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Plus(double o1, double o2)
+        {
+            double result = o1 + o2;
+            ViewBag.Result = $"{o1} + {o2} = {result}";
+            return View();
+        }
+
+        public IActionResult Minus(double o1, double o2)
+        {
+            double result = o1 - o2;
+            ViewBag.Result = $"{o1} - {o2} = {result}";
+            return View();
+        }
+
+        public IActionResult Multiply(double o1, double o2)
+        {
+            double result = o1 * o2;
+            ViewBag.Result = $"{o1} * {o2} = {result}";
+            return View();
+        }
+
+        public IActionResult Divide(double o1, double o2)
+        {
+            double result = o1 / o2;
+            ViewBag.Result = $"{o1} / {o2} = {result}";
+            return View();
+        }
+    }
+}
